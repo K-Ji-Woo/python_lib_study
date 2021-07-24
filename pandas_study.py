ae6ac7d75634e6efd0f6028f775c3422f075a6f5
K-Ji-Woo <rlawldn1018@naver.com>
2021-07-24 19:36:40 +0900

    DataFrame의 생성 및 주요 속성 & 오타 수정

--- pandas_study.py
+++ pandas_study.py
@@ -30,7 +30,26 @@ print(sample_seires)
 print('\n Series객체의 주요 속성')
 print(sample_seires.index) # 인덱스 정보
 print(sample_seires.index.name) # 인덱스의 이름
-print(sample_seires.name) # column의 이름(한개의 행 밖에 없지만)
+print(sample_seires.name) # column의 이름(한개의 열 밖에 없지만)
 print(sample_seires.values) # 저장된 데이터
 print(sample_seires.dtype) # 저장된 데이터의 데이터형
 print(sample_seires.shape) # 객체의 형태
+
+
+# Pandas의 자료구조 - DataFrame
+# 2차원 자료구조(테이블 형태), 여러개의 Series가 결합된 형태, 1개의 열은 1개의 Series
+# DataFrame의 생성
+score = {'name': ['Jessi', 'Emma', 'Alex', 'Tom'],
+         'score': [100, 95, 80, 85],
+         'grade': ['A', 'A', 'B', 'B']}
+sample_df = pd.DataFrame(score)
+print("\nDataFrame 생성\n", sample_df)
+
+
+# DataFrame의 주요 속성
+print("\nDataFrame객체의 주요 속성")
+print(sample_df.index) # 인덱스
+print(sample_df.columns) # 칼럼
+print(sample_df.values) # 저장된 데이터
+print(sample_df.dtypes) # 데이터 타입
+print(sample_df.shape) # 데이터 형태
